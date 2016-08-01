69b10b2c6499b8f7930d6efa2bbfaadb04c4c495
kushti <alex.chepurnoy@iohk.io>
2016-08-01 17:20:57 +0300

    TestUtils styling

--- src/test/scala/io/iohk/iodb/TestUtils.scala
+++ src/test/scala/io/iohk/iodb/TestUtils.scala
@@ -12,37 +12,34 @@ object TestUtils {
 
   def dirSize(dir: File): Long = dir.listFiles().map(_.length()).sum
 
-
   def tempDir(): File = {
     val dir = new File(System.getProperty("java.io.tmpdir") + "/iodb" + Math.random())
     dir.mkdirs()
-    return dir
+    dir
   }
 
   def deleteRecur(dir: File): Unit = {
     if (dir == null) return
     val files = dir.listFiles()
     if (files != null)
-      files.foreach(deleteRecur(_))
+      files.foreach(deleteRecur)
     dir.delete()
   }
 
-  private val random = new Random();
-
   /** generates random byte[] of given size */
-  def randomA(size: Int = 32, random:Random=this.random): ByteArrayWrapper = {
+  def randomA(size: Int = 32): ByteArrayWrapper = {
     val b = new Array[Byte](size)
-    random.nextBytes(b)
-    new ByteArrayWrapper(b)
+    Random.nextBytes(b)
+    ByteArrayWrapper(b)
   }
 
   /** return value of  `-DlongTest=1` property, 0 if value is not defined, or 1 if its defined but not a number */
-  def longTest():Long = {
+  def longTest(): Long = {
     var ret = 0L
     try {
       val prop = System.getProperty("longTest")
-      if(prop!=null) {
-        ret = 1;
+      if (prop != null) {
+        ret = 1
         if (prop.matches("[0-9]+")) {
           ret = prop.toLong
         }
@@ -51,8 +48,7 @@ object TestUtils {
       case _: Exception =>
     }
 
-    return ret;
-
+    ret
   }
 
   /** Duration of long running tests.
@@ -60,21 +56,20 @@ object TestUtils {
     * By default tests run 5 seconds, but it can be increased by setting this property.
     */
   def endTimestamp(): Long = {
-    var end: Long = 5 * 1000 + longTest() * 60*1000;
-
-    return end + System.currentTimeMillis()
+    var end: Long = 5 * 1000 + longTest() * 60 * 1000
+    end + System.currentTimeMillis()
   }
 
   /** measures time it takes to execute function */
   def stopwatch(e: () => Unit): Long = {
     val s = System.currentTimeMillis()
     e.apply()
-    return System.currentTimeMillis() - s
+    System.currentTimeMillis() - s
   }
 
-  def fromLong(id:Long):ByteArrayWrapper ={
+  def fromLong(id: Long): ByteArrayWrapper = {
     val b = ByteBuffer.allocate(8)
     b.putLong(0, id)
-    return new ByteArrayWrapper(b.array())
+    ByteArrayWrapper(b.array())
   }
 }
