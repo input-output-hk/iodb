cc1746513c62fbbdab74a40cb700a49a18c8bef9
catena <catena@protonmail.com>
2017-03-16 16:12:00 +0300

    Complex rollback test

--- src/test/scala/io/iohk/iodb/prop/IODBSpecification.scala
+++ src/test/scala/io/iohk/iodb/prop/IODBSpecification.scala
@@ -18,6 +18,63 @@ class IODBSpecification extends PropSpec
   val iFile = TestUtils.tempDir()
   iFile.mkdirs()
 
+  property("rollback test") {
+    //initialize test
+    val NumberOfBlocks = 100
+    val NumberOfRollbacks = 100
+
+    case class BlockChanges(id: ByteArrayWrapper,
+                            toRemove: Seq[ByteArrayWrapper],
+                            toInsert: Seq[(ByteArrayWrapper, ByteArrayWrapper)])
+
+    def hash(b: Array[Byte]): Array[Byte] = MessageDigest.getInstance("SHA-256").digest(b)
+
+    def randomBytes(): ByteArrayWrapper = ByteArrayWrapper(hash(Random.nextString(16).getBytes))
+
+    def generateBytes(): Seq[(ByteArrayWrapper, ByteArrayWrapper)] = {
+      (0 until Random.nextInt(100)).map(i => (randomBytes(), randomBytes()))
+    }
+
+    val blockchain: IndexedSeq[BlockChanges] = {
+      def loop(acc: IndexedSeq[BlockChanges], existingKeys: Seq[ByteArrayWrapper]): IndexedSeq[BlockChanges] = {
+        if (acc.length < NumberOfBlocks) {
+          val toInsert = generateBytes()
+          val toRemove: Seq[ByteArrayWrapper] = existingKeys.filter(k => Random.nextBoolean())
+          val newExistingKeys = existingKeys.filter(ek => !toRemove.contains(ek)) ++ toInsert.map(_._1)
+          val newBlock = BlockChanges(randomBytes(), toRemove, toInsert)
+          loop(newBlock +: acc, newExistingKeys)
+        } else {
+          acc
+        }
+      }
+      loop(IndexedSeq.empty, Seq.empty)
+    }
+    val allBlockchainKeys: Seq[ByteArrayWrapper] = blockchain.flatMap(_.toInsert.map(_._1))
+
+    def storageHash(storage: LSMStore): Array[Byte] = {
+      val valuesBytes = allBlockchainKeys.map(k => storage.get(k).map(_.toString).getOrElse("")).mkString.getBytes
+      hash(valuesBytes)
+    }
+
+    //Start test
+    val blockStorage = new LSMStore(iFile)
+    //initialize blockchain
+    blockchain.foreach(b => blockStorage.update(b.id, b.toRemove, b.toInsert))
+
+    val finalHash = storageHash(blockStorage)
+    val finalVersion = blockStorage.lastVersionID.get
+
+    (0 until NumberOfRollbacks) foreach { _ =>
+      val idToRollback: ByteArrayWrapper = blockchain.map(_.id).apply(Random.nextInt(blockchain.length))
+      val index: Int = blockchain.indexWhere(_.id == idToRollback)
+      val removedBlocks = blockchain.takeRight(NumberOfBlocks - index - 1)
+      blockStorage.rollback(idToRollback)
+
+      removedBlocks.foreach(b => blockStorage.update(b.id, b.toRemove, b.toInsert))
+      blockStorage.lastVersionID.get shouldEqual finalVersion
+      storageHash(blockStorage) shouldEqual finalHash
+    }
+  }
 
   property("writeKey test") {
     val blocksStorage = new LSMStore(iFile)
@@ -59,23 +116,6 @@ class IODBSpecification extends PropSpec
       }
     }
     ids.foreach(id => blocksStorage.rollbackVersions().exists(_ == id) shouldBe true)
-
-  }
-
-  property("rollback test") {
-    val blocksStorage = new LSMStore(iFile)
-    val initialVersion = blocksStorage.lastVersionID.get
-    forAll { (key: String, value: Array[Byte]) =>
-
-      val id = hash(new String(value) + key)
-      val fValue: ByteArrayWrapper = ByteArrayWrapper(value)
-
-      blocksStorage.update(id, Seq(), Seq(id -> fValue))
-      blocksStorage.lastVersionID.get should not equal initialVersion
-      blocksStorage.rollback(initialVersion)
-      blocksStorage.lastVersionID.get shouldEqual initialVersion
-
-    }
 
   }
 
